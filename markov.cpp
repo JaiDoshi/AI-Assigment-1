8e55ce3427d156bd100777fd71a01d5aa43f60b9
arshitmodi <f20180191@pilani.bits-pilani.ac.in>
2021-03-26 13:21:29 +0530

    rough markov.cpp

---
+++ markov.cpp
@@ -0,0 +1,63 @@
+#include <vector>
+#include <iostream>
+#include<math.h>
+#include "matrix.cpp"
+using namespace std;
+
+class State{
+  int m;
+  Matrix s;
+  Matrix transProb;
+  State(int m){
+    this->m = m;
+    s.mat.resize(m,vector<double>(1));
+    cout << "Enter the probabilities of state" ;
+    s.take_input();
+    transProb.mat.resize(m,vector<double>(m));
+    cout << "Enter the values in transition probability matrix";
+    transProb.take_input();
+  }
+
+
+  State nth(State initial , Matrix transProb){
+    State fin = initial;
+    for(int i = 0 ; i<initial.m ; i++){
+      fin.s = mult(fin.s,transProb);
+    }
+    return fin;
+  }
+
+  State eq(State initial , Matrix transProb){
+    State fin = initial;
+    State prev = fin;
+    Matrix dev(m,1) ;
+    int i = 1;
+    while(dev[i-1] >= 0.0001){
+      prev.s = fin.s;
+      fin.s = mult(fin.s,transProb);
+      dev.mat[i][1] = calcDev(fin,prev);
+    }
+  }
+  double calcDev(Matrix & s1, Matrix & s2 ){
+
+    if(s1.n != 1 || s2.n != 1){
+      return 1.00;
+    }
+    Matrix dev(s1.m,1);
+    double res = 0;
+    for(int i = 0 ; i<s1.m ; i++){
+      res += (s1.mat[i][1] - s2.mat[i][1])^2;
+    }
+    return res;
+  }
+
+}
+
+
+double prob(vector<int> v, Matrix transProb){
+  double res = 1;
+  for(int i = 1; i <v.size(); i++){
+    res *= transProb[v[i-1]][v[i]];
+  }
+  return res;
+}
